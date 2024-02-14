61e8578f34d0b183113123207905d53468b02746
Enno Richter <enno@nerdworks.de>
2024-02-14 17:11:08 +0100

    expose psm

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <tesseract/baseapi.h>
 
 namespace py = pybind11;
+using tesseract::PageSegMode;
 using tesseract::TessBaseAPI;
 
 int add(int i, int j) { return i + j; }
@@ -22,6 +23,13 @@ PYBIND11_MODULE(tessbind, m) {
              return std::unique_ptr<TessBaseAPI>(api);
            }),
            py::arg("datapath"), py::arg("language"))
+      .def("End", &TessBaseAPI::End,
+           "Close down tesseract and free up all memory, after which the "
+           "instance should not be reused.")
+      .def_property(
+          "pageSegMode", &TessBaseAPI::GetPageSegMode,
+          &TessBaseAPI::SetPageSegMode,
+          R"pbdoc(This attribute can be used to get or set the page segmentation mode used by the tesseract model)pbdoc")
       .def_property_readonly(
           "utf8Text", &TessBaseAPI::GetUTF8Text,
           R"pbdoc(Read-only: Return all identified text concatenated into a UTF-8 string)pbdoc")
@@ -41,4 +49,37 @@ PYBIND11_MODULE(tessbind, m) {
             api.SetImage(image);
           },
           py::arg("bytes"), "Read an image from a string of bytes");
+
+  py::enum_<PageSegMode>(m, "PageSegMode",
+                         "Enumeration of page segmentation settings")
+      .value("OSD_ONLY", PageSegMode::PSM_OSD_ONLY,
+             "Segment the page in \"OSD only\" mode")
+      .value("AUTO_OSD", PageSegMode::PSM_AUTO_OSD,
+             "Segment the page in \"Auto OSD\" mode")
+      .value("AUTO_ONLY", PageSegMode::PSM_AUTO_ONLY,
+             "Segment the page in \"Automatic only\" mode")
+      .value("AUTO", PageSegMode::PSM_AUTO,
+             "Segment the page in \"Automatic\" mode")
+      .value("SINGLE_COLUMN", PageSegMode::PSM_SINGLE_COLUMN,
+             "Segment the page in \"Single column\" mode")
+      .value("SINGLE_BLOCK_VERT_TEXT", PageSegMode::PSM_SINGLE_BLOCK_VERT_TEXT,
+             "Segment the page in \"Single block of vertical text\" mode")
+      .value("SINGLE_BLOCK", PageSegMode::PSM_SINGLE_BLOCK,
+             "Segment the page in \"Single block\" mode")
+      .value("SINGLE_LINE", PageSegMode::PSM_SINGLE_LINE,
+             "Segment the page in \"Single line\" mode")
+      .value("SINGLE_WORD", PageSegMode::PSM_SINGLE_WORD,
+             "Segment the page in \"Single word\" mode")
+      .value("CIRCLE_WORD", PageSegMode::PSM_CIRCLE_WORD,
+             "Segment the page in \"Circle word\" mode")
+      .value("SINGLE_CHAR", PageSegMode::PSM_SINGLE_CHAR,
+             "Segment the page in \"Single character\" mode")
+      .value("SPARSE_TEXT", PageSegMode::PSM_SPARSE_TEXT,
+             "Segment the page in \"Sparse text\" mode")
+      .value("SPARSE_TEXT_OSD", PageSegMode::PSM_SPARSE_TEXT_OSD,
+             "Segment the page in \"Sparse text OSD\" mode")
+      .value("RAW_LINE", PageSegMode::PSM_RAW_LINE,
+             "Segment the page in \"Raw line\" mode")
+      .value("COUNT", PageSegMode::PSM_COUNT,
+             "Segment the page in \"Count\" mode");
 }
