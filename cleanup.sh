186d485af0b82e37a0a37d4342883cf6f70164f1
Enno Richter <enno@nerdworks.de>
2025-02-20 09:34:14 +0100

    add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "This will remove all untracked files in this repo and extern repos."
+echo "Are you sure you want to continue? [y/N] "
+read -r response
+
+if [[ "$response" =~ ^[Yy]$ ]]; then
+    # Clean main repo
+    git clean -fdx
+
+    # Clean extern repos
+    pushd extern/tesseract && git clean -fdx && popd || exit 1
+    pushd extern/leptonica && git clean -fdx && popd || exit 1
+else
+    echo "Cleanup cancelled"
+    exit 0
+fi
