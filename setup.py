063b2b93ba7e1024491c1f3cf4dc15bd5ab73853
Enno Richter <enno@nerdworks.de>
2025-02-20 09:49:59 +0100

    update setup/manifest code - call build from setup.py, set rpaths

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@
 
 from __future__ import annotations
 
+import subprocess
 import sys
 from pathlib import Path
 
 from setuptools import setup  # isort:skip
+from setuptools.command.build_ext import build_ext
 
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension  # isort:skip
@@ -35,21 +37,42 @@ ext_modules = [
 ]
 
 
-# Add PNG paths
+# Add macOS-specific link arguments
 if sys.platform == "darwin":
-    from subprocess import check_output
-
-    png_prefix = check_output(["brew", "--prefix", "libpng"]).decode().strip()
+    png_prefix = (
+        subprocess.check_output(["brew", "--prefix", "libpng"]).decode().strip()
+    )
     for ext in ext_modules:
+        # Add RPATH entries
+        ext.extra_link_args = [
+            "-Wl,-rpath,@loader_path/../../extern/leptonica/leptonica-install/lib",
+            "-Wl,-rpath,@loader_path/../../extern/tesseract/tesseract-install/lib",
+        ]
+
+        # Add PNG paths
         ext.include_dirs.append(str(Path(png_prefix) / "include"))
         ext.library_dirs.append(str(Path(png_prefix) / "lib"))
         ext.libraries.append("png")
 elif sys.platform == "linux":
-    # On Linux, libpng-dev installs to system paths
     for ext in ext_modules:
+        # Add RPATH entries for Linux
+        ext.extra_link_args = [
+            "-Wl,-rpath,$ORIGIN/../../extern/leptonica/leptonica-install/lib",
+            "-Wl,-rpath,$ORIGIN/../../extern/tesseract/tesseract-install/lib",
+        ]
+
+        # On Linux, libpng-dev installs to system paths
         ext.libraries.append("png")
+
+
+class CustomBuildExt(build_ext):
+    def run(self):
+        # Build dependencies first
+        subprocess.check_call(["./build_dependencies.sh"])
+        super().run()
 
 
 setup(
     ext_modules=ext_modules,
+    cmdclass={"build_ext": CustomBuildExt},
 )
